b6589431964fde2b3cd0fcf98ecebea2e18a6f4d
Kseniia Des <kseniya250496@mail.ru>
2022-11-25 13:21:41 +0300

    Add Task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Squares(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.WriteLine($"{i, 4}    {i * i, 4}");
+    }
+}
+
+if (number > 0) Squares(number);
+else Console.WriteLine("Число не является натуральным");
